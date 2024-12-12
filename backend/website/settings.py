b41430529faa7f25050d3a92135161fa5082348d
Ellio Hickman <ellio.hickman001@gmail.com>
2024-12-12 12:51:16 +0100

    Update : modified DATABASE settings to handle PostgreSQL

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -72,11 +72,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/5.1/ref/databases/#postgresql-notes
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
