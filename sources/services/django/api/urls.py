85d7a4d136863c29851f804dac0ac2fbcf0a44ce
Ellio Hickman <ellio.hickman001@gmail.com>
2024-12-18 16:43:11 +0100

    Update : fixed catch all url

--- sources/services/django/api/urls.py
+++ sources/services/django/api/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('token-auth/', views.CustomAuthToken.as_view(), name='api_token_auth'),
 	path("users/", views.UserList.as_view(), name="user-list"),
 	path("users/<int:pk>", views.UserDetail.as_view(), name="user-detail"),
-	re_path(r'^frontend/(?P<path>.*)$', views.frontend, name="frontend-file"),
-	re_path(r'^(?!api|admin).*$', views.index, name='index'),
 ]
