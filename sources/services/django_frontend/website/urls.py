29314c763176c2fd1a8a9ad51631eb1e639908bf
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-01-16 16:48:24 +0100

    Update: redirect working

--- sources/services/django_frontend/website/urls.py
+++ sources/services/django_frontend/website/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from api import views
+from . import views
 
 urlpatterns = [
     # Admin URLs
