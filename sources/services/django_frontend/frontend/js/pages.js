687562a1feff201816572f487c3256d0eb3af61f
slqye <uduterrage@gmail.com>
2025-01-21 18:21:45 +0100

    Add: starting history

--- sources/services/django_frontend/frontend/js/pages.js
+++ sources/services/django_frontend/frontend/js/pages.js
@@ -175,28 +175,8 @@ async function load_profile() {
 	if (template == null)
 		return console.error(ERROR_TEMPLATE);
 	load_navbar();
-	try
-	{
-		const response = await fetch("/api/users/me/", {
-			method: "GET",
-			headers:
-			{
-				"Authorization": `Token ${localStorage.getItem("auth-token")}`,
-				"Content-Type": "application/json"
-			}
-		});
-		if (!response.ok)
-		{
-			new Toast(Toast.ERROR, "A network error occurred.");
-			throw new Error("A network error occurred.");
-		}
-		const data = await response.json();
-		template.edit.id.set.content("profile_user_name", "@" + data.username);
-	}
-	catch (error)
-	{
-		new Toast(Toast.ERROR, error);
-	}
+	set_profile(template);
+	set_profile_history(template);
 	content.innerHTML = template.string;
 	if (window.location.pathname !== "/profile")
 		history.pushState({ page: "profile" }, "Profile", "/profile");
