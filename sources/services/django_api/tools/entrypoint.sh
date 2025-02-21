622850fbb4af4508844bc05a039169636b257bfb
Ellio Hickman <ellio.hickman001@gmail.com>
2025-02-21 12:49:49 +0100

    Fixed: script issue not making migrations

--- sources/services/django_api/tools/entrypoint.sh
+++ sources/services/django_api/tools/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-exec gunicorn --bind 0.0.0.0:8081 website.wsgi:application
-
-python manage.py makemigrations
+python manage.py makemigrations api
 python manage.py migrate
+
+exec gunicorn --bind 0.0.0.0:8081 website.wsgi:application
