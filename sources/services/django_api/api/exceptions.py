3f3f2dc692cceb5f2c3d7a05b1afc5696a6d79a9
Ellio Hickman <ehickman@k0r3p14.42mulhouse.fr>
2025-02-24 15:06:09 +0100

    Fix: untranslated error messages

--- sources/services/django_api/api/exceptions.py
+++ sources/services/django_api/api/exceptions.py
@@ -1,13 +1,43 @@
 from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc, context):
-    response = exception_handler(exc, context)
-    if response is not None and isinstance(response.data, dict):
-        detail_messages = []
-        for key, value in response.data.items():
-            if isinstance(value, list):
-                detail_messages.extend(value)
-            else:
-                detail_messages.append(str(value))
-        response.data = {"detail": " ".join(detail_messages)}
-    return response
+	response = exception_handler(exc, context)
+	request = context.get('request')
+	if response is not None and isinstance(response.data, dict):
+		detail_messages = []
+		for key, value in response.data.items():
+			if isinstance(value, list):
+				detail_messages.extend(value)
+			else:
+				detail_messages.append(str(value))
+		messages = translate_message(detail_messages, request)
+		response.data = {"detail": " ".join(messages)}
+	return response
+
+def translate_message(error_messages, request):
+	preferred_lang = request.headers.get("Preferred-Language", "en")
+	if preferred_lang not in ("en", "fr", "de"):
+		preferred_lang = "en"
+	print(preferred_lang)
+	translations = {
+		"user with this username already exists.": {
+			"en": "user with this username already exists.",
+			"fr": "Un utilisateur avec ce nom d'utilisateur existe déjà.",
+			"de": "Ein Benutzer mit diesem Benutzernamen existiert bereits."
+		},
+		"user with this display name already exists.": {
+			"en": "user with this display name already exists.",
+			"fr": "Un utilisateur avec ce nom d'affichage existe déjà.",
+			"de": "Ein Benutzer mit diesem Anzeigenamen existiert bereits."
+		}
+	}
+	translated_messages = []
+	for message in error_messages:
+		if message == "user with this username already exists.":
+			localized_message = translations["user with this username already exists."].get(preferred_lang, message)
+			translated_messages.append(localized_message)
+		elif message == "user with this display name already exists.":
+			localized_message = translations[message].get(preferred_lang, message)
+			translated_messages.append(localized_message)
+	
+	return translated_messages
